608643f378d909a24003d5e3f9b0314bee0acbeb
Abraham <ekekhorabraham@gmail.com>
2020-06-22 23:36:54 +0100

    Added some stuff

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 'use strict';
-
 const http = require('http');
 const fs = require('fs');
-
 const express = require('express');
 const multer = require('multer');
-const csv = require('fast-csv');
-
-const Router = express.Router;
+//used to parse csv files
+const csv = require('csv-parser');
+// used to get environment variables
+require('dotenv').config()
 
 const app = express();
-const router = new Router();
 const server = http.createServer(app);
-const port = 9000
+const port = 9000 || process.env.PORT;
+let defaultExtension = 'csv';
 
 // SET STORAGE
 var storage = multer.diskStorage({
@@ -20,33 +19,82 @@ var storage = multer.diskStorage({
     cb(null, 'uploads')
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
+    cb(null, file.fieldname)
   }
 })
  
 var upload = multer({ storage: storage })
 
-app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+
+const fileVerification = function (req, res, next){
+  //get the sent file
   const file = req.file
+  //check if a file was sent
   if (!file) {
-    const error = new Error('Please upload a file')
-    error.httpStatusCode = 400
-    return next(error)
+    //return status code and error message
+    return res.status(400).send({
+      status: false,
+      message: 'Please upload a File'
+    })
   }
-    res.send(file)
-  
-})
-
-app.get('/',function(req,res){
-  res.sendFile(__dirname + '/src/upload.html');
-
-});
-
-// Start server
-function startServer() {
-  server.listen(port, function () {
-    console.log('Express server listening on ', port);
-  });
+  //get the file extension
+  let index = file.originalname.indexOf(".");
+  let extension = file.originalname.slice(index+1);
+  //check if the extension is accepted
+  if (extension.toLowerCase() != 'csv' && extension.toLowerCase() != 'json') {
+    res.status(400).send({
+      status: false,
+      message: 'Invalid File Format'
+    })
+  }else{
+    //set defaultExtension to the file extension
+    defaultExtension = extension
+    next()
+  }
 }
 
-setImmediate(startServer);
+const displayLeaderBoard = function(req, res){
+  res.sendFile(__dirname + '/src/leaderboard.html');
+}
+
+const sendFileData = function(req, res){
+  //check the extension
+  if (defaultExtension.toLowerCase() == 'csv') {
+    var data =[]
+    //read the file
+    fs.createReadStream('uploads/myFile')
+    //pass the file through a csv parser
+    .pipe(csv())
+    //on the load of each row, add it to an array
+    .on('data', (row) => {
+      data.push(row)
+    })
+    //after loading the data send it to the client
+    .on('end', () => {
+      console.log('CSV file successfully processed');
+      res.send(data)
+    }); 
+  }
+  else if (defaultExtension.toLowerCase() == 'json') {
+    //get the raw data from the json file
+    let rawdata = fs.readFileSync('uploads/myFile');
+    //parse to a json format
+    let jsonData = JSON.parse(rawdata);
+    //get the data array
+    let data =(jsonData.data)
+    //send the data
+    res.send(data)    
+  }
+}
+
+//ROUTES
+app.route('/uploadfile').post(upload.single('myFile'), fileVerification, displayLeaderBoard);
+app.route('/data').get(sendFileData)
+app.route('/').get(function(req, res){
+  res.sendFile(__dirname + '/src/upload.html');
+
+})
+
+app.listen(port, function(){
+  console.log(`Express server listening on port ${port}`);
+})
